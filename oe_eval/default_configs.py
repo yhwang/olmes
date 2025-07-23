86fbb4b18c03e9d18ef03c0fb33154f76eb82630
Yotam Perlitz <y.perlitz@ibm.com>
2025-07-23 16:54:27 +0200

    add extra headers to support RITS_API_KEY
    Signed-off-by: Yotam Perlitz <y.perlitz@ibm.com>

--- oe_eval/default_configs.py
+++ oe_eval/default_configs.py
@@ -27,6 +27,7 @@ MODEL_DEFAULTS = {
     "gpu_memory_utilization": None,
     "process_output": None,
     "api_base_url": None,  # Used for litellm models
+    "extra_headers": None,  # Used for litellm models
 }
 
 TASK_DEFAULTS = {
